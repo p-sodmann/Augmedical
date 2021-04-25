6f89bed2af6e16f1324596db0d6a6660ff2772e0
Phil <psodmann@gmail.com>
2021-04-25 14:55:21 +0200

    added randaug

--- augmedical/randaugment/randaugment.py
+++ augmedical/randaugment/randaugment.py
@@ -1,2 +1,16 @@
+import numpy as np
+
+
 class RandAugment:
-    
+    def __init__(self, augmentations, n, m):
+        self.augmentations = augmentations
+        self.n = n
+        self.intensity = m
+
+    def __call__(self, sample):
+        sampled_ops = np.random.choice(self.augmentations, self.n)
+
+        for op in sampled_ops:
+            sample = op(sample, self.intensity)
+
+        return sample
